443607abe8f55a7bd265a98b27076774ec7c81e6
Hammad Shoaib Khan <khanhammad921@gmail.com>
2021-08-26 11:46:15 +0500

    login register page added, admin panel vendors listing page added

---
+++ resources/views/admin/vendors/index.blade.php
@@ -0,0 +1,78 @@
+@extends('layouts.admin.master')
+@section('content')
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Vendors</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="/">Home</a></li>
+              <li class="breadcrumb-item active">Vendors</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+            <div class="col-10">
+            </div>
+            <div class="col-2">
+                <button type="button" class="btn btn-block btn-primary btn-sm">Add Vendor</button>
+            </div>
+        </div>
+        <br>
+
+        <div class="row">
+          <div class="col-12">
+            <div class="card">
+              <!-- /.card-header -->
+              <div class="card-body">
+                <table id="datatable" class="table table-bordered table-striped">
+                  <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Status</th>
+                    <th>Action</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  <tr>
+                    <td>Trident</td>
+                    <td>Internet
+                      Explorer 4.0
+                    </td>
+                    <td>Win 95+</td>
+                    <td> 
+                        <a href="javascript:;" class="btn btn-sm btn-info"><i class="fas fa-pencil-alt"></i></a>
+                        <a href="javascript:;" class="btn btn-sm btn-danger"><i class="fas fa-trash"></i></a>
+                    </td>
+                  </tr>
+                  </tbody>
+                </table>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+          <!-- /.col -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+@endsection
--- resources/views/layouts/admin/includes/sidebar.blade.php
+++ resources/views/layouts/admin/includes/sidebar.blade.php
@@ -26,6 +26,14 @@
               </p>
             </a>
           </li>
+          <li class="nav-item">
+            <a href="{{ route('admin.vendors') }}" class="nav-link">
+              <i class="nav-icon fas fa-copy"></i>
+              <p>
+                Vendors
+              </p>
+            </a>
+          </li>
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
