5e31ee5c5d704883b8ac56cdf13b182e6d7d71e3
HAFIZ MUHAMMAD AHTSHAM AHMAD ZAHOOR <61229568+ahtshambhatti@users.noreply.github.com>
2021-10-10 22:46:51 +0500

    order view updated

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -14,15 +14,20 @@ class OrderController extends Controller
         $orders = Order::orderBy('id', 'DESC')->get();
         return view('admin.orders.index', compact('title','orders'));
     }
+
     function orderView($id=null){
-        dd($id);
-        return null;
+        if ($id != null && Order::where('id', $id)->exists()) {
+            $title = "Orders";
+            $order = Order::find($id);
+            return view('admin.orders.view', compact('title', 'order'));
+        }
+        return redirect()->route('admin.orders');
     }
+
     public function changeStatus(Request $request)
     {
         Order::whereIn('id', $request->order_ids)->update(['status' => $request->status]);
         $orders = Order::orderBy('id', 'DESC')->get();
         return view('admin.orders.partials.order-listings', compact('orders'));
     }
-
 }
--- resources/views/admin/orders/view.blade.php
+++ resources/views/admin/orders/view.blade.php
@@ -13,7 +13,8 @@
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
                             <li class="breadcrumb-item"><a href="/">Home</a></li>
-                            <li class="breadcrumb-item active">Orders</li>
+                            <li class="breadcrumb-item"><a href="{{route('admin.orders')}}">Orders</a></li>
+                            <li class="breadcrumb-item active">View Order</li>
                         </ol>
                     </div>
                 </div>
@@ -31,47 +32,112 @@
                         <div class="card">
                             <!-- /.card-header -->
                             <div class="card-body" id="bind-orders">
-                                <table id="datatable" class="table table-bordered table-striped">
-                                    <thead>
-                                    <tr>
-                                        <th><input type="checkbox" class="select-all-checkbox"></th>
-                                        <th>Email</th>
-                                        <th>Total</th>
-                                        <th>Discount</th>
-                                        <th>Paid</th>
-                                        <th>Status</th>
-                                        <th>Action</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    @forelse($orders as $key => $order)
-                                        <tr>
-                                            <td><input type="checkbox" class="checkboxes" data-id="{{ $order->id }}"></td>
-                                            <td>{{ $order->user_email ?? '' }}</td>
-                                            <td>{{ $order->total_bill ?? 0 }}</td>
-                                            <td>{{ $order->total_discount ?? 0 }}</td>
-                                            <td>
+                                <div class="form-group">
+                                    <div class="row">
+                                        <label class="col-sm-3 form-control-plaintext">
+                                            Name
+                                        </label>
+                                        <div class="col-sm-5">
+                                            <p class="form-control-plaintext">
+                                                @empty(!$order->user_id)
+                                                    {{ getuserName($order->user_id) ?? 'User is Not Registered Yet' }}                                                    
+                                                @endempty
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>                                
+                                <div class="form-group">
+                                    <div class="row">
+                                        <label class="col-sm-3 form-control-plaintext">
+                                            Email
+                                        </label>
+                                        <div class="col-sm-5">
+                                            <p class="form-control-plaintext" >
+                                                {{ $order->user_email ?? '' }}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="row">
+                                        <label class="col-sm-3 form-control-plaintext">
+                                            User Detail
+                                        </label>
+                                        <div class="col-sm-5">
+                                            <p class="form-control-plaintext" >
+                                                {{ $order->user_detail ?? 0 }}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="row">
+                                        <label class="col-sm-3 form-control-plaintext">
+                                            Order Detail
+                                        </label>
+                                        <div class="col-sm-5">
+                                            <p class="form-control-plaintext" >
+                                                {{ $order->order_detail ?? 0 }}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="row">
+                                        <label class="col-sm-3 form-control-plaintext">
+                                            Total Discount
+                                        </label>
+                                        <div class="col-sm-5">
+                                            <p class="form-control-plaintext" >
+                                                {{ $order->total_discount ?? 0 }}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="row">
+                                        <label class="col-sm-3 form-control-plaintext">
+                                            Total Paid
+                                        </label>
+                                        <div class="col-sm-5">
+                                            <p class="form-control-plaintext"> 
+                                                {{ $order->total_bill  ?? 0 }}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="row">
+                                        <label class="col-sm-3 form-control-plaintext">
+                                            Amount Paid
+                                        </label>
+                                        <div class="col-sm-5">
+                                            <p class="form-control-plaintext">  
                                                 @if ($order->paid == 1)
                                                     <span class="badge badge-success">YES</span>
                                                 @else
                                                     <span class="badge badge-danger">NO</span>
-                                                @endif
-                                            </td>
-                                            <td>
+                                                @endif 
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="row">
+                                        <label class="col-sm-3 form-control-plaintext">
+                                            Status
+                                        </label>
+                                        <div class="col-sm-5">
+                                            <p class="form-control-plaintext">  
                                                 @if ($order->status == 1)
                                                     <span class="badge badge-success">Active</span>
                                                 @else
                                                     <span class="badge badge-danger">Disabled</span>
-                                                @endif
-                                            </td>
-                                            <td>
-                                                <a href="{{ route('admin.order.view', $order->id) }}" class="btn btn-sm btn-info"><i class="fas fa-eye"></i></a>
-                                            </td>
-                                        </tr>
-                                    @empty
-                                    @endforelse
-                                    </tbody>
-                                </table>
+                                                @endif 
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                             <!-- /.card-body -->
                         </div>
