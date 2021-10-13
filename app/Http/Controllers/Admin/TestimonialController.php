6d1585e039911861b6e157054a5f8327617df5fe
HAFIZ MUHAMMAD AHTSHAM AHMAD ZAHOOR <61229568+ahtshambhatti@users.noreply.github.com>
2021-10-13 13:31:46 +0500

    Create TestimonialController.php

---
+++ app/Http/Controllers/Admin/TestimonialController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TestimonialController extends Controller
+{
+    function index(){
+        echo 'Halo World';
+    }
+    function testimonialView(){
+
+    }
+    function changeStatus(){
+
+    }
+}
