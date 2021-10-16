05ba5da79e0b86a3ceb782f07c6038dbde2f8a58
HAFIZ MUHAMMAD AHTSHAM AHMAD ZAHOOR <61229568+ahtshambhatti@users.noreply.github.com>
2021-10-16 13:18:49 +0500

    Update TestimonialController.php

--- app/Http/Controllers/Admin/TestimonialController.php
+++ app/Http/Controllers/Admin/TestimonialController.php
@@ -4,19 +4,45 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Testimonial;
 
 class TestimonialController extends Controller
 {
     function index(){
-        echo 'Halo World';
+        $title = "Testimonials";
+        $testimonials = Testimonial::orderBY('id', 'DESC')->get();
+        return view('admin.testimonials.index', compact('title','testimonials'));
     }
-    function create(){
-        dd('create');
-    }
-    function testimonialView(){
 
+    function testimonialView($id=null){
+        if ($id != null && Testimonial::where('id', $id)->exists()) {
+            $title = "Testimonials";
+            $testimonial = Testimonial::find($id);
+            return view('admin.testimonials.view', compact('title', 'testimonial'));
+        }
+        return redirect()->route('admin.testimonials');
     }
-    function changeStatus(){
+    
+    public function delete(Request $request)
+    {
+        $user = Testimonial::find($request->testimonial_id);
+        $user->delete();
+        $testimonials = Testimonial::orderBY('id', 'DESC')->get();
+        return view('admin.testimonials.partials.testimonial-listings', compact('testimonials'));
+    }
+    
+    public function multipleDelete(Request $request)
+    {
+        Testimonial::whereIn('id', $request->testimonial_ids)->delete();
+        $testimonials = Testimonial::orderBY('id', 'DESC')->get();
+        return view('admin.testimonials.partials.testimonial-listings', compact('testimonials'));
+    }
 
+    public function changeStatus(Request $request)
+    {
+        testimonial::whereIn('id', $request->testimonial_ids)->update(['approved' => $request->approved]);
+        $testimonials = Testimonial::orderBY('id', 'DESC')->get();
+        return view('admin.testimonials.partials.testimonial-listings', compact('testimonials'));
     }
+    
 }
