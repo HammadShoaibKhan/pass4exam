89e195c27e4bbe75995986bad03dbf70e6867a2b
HAFIZ MUHAMMAD AHTSHAM AHMAD ZAHOOR <61229568+ahtshambhatti@users.noreply.github.com>
2021-10-20 12:01:21 +0500

    Create index.blade.php

---
+++ resources/views/admin/contentManager/index.blade.php
@@ -0,0 +1,74 @@
+@extends('layouts.admin.master')
+@section('content')
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>contentManagers</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="/">Home</a></li>
+                            <li class="breadcrumb-item active">contentManagers</li>
+                        </ol>
+                    </div>
+                </div>
+            </div><!-- /.container-fluid -->
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-9">
+                    </div>
+                    <div class="col-3">
+                        <a href="{{ route('admin.content.create') }}" class="btn btn-block btn-primary btn-md"><i class="fa fa-plus"></i>&nbsp;&nbsp; Add&nbspPage Content</a>
+                    </div>
+                </div>
+                <br>
+
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <!-- /.card-header -->
+                            <div class="card-body" id="bind-contentManagers">
+                                <table id="datatable" class="table table-bordered table-striped">
+                                    <thead>
+                                    <tr>
+                                        <th>Page Title</th>
+                                        <th>Action</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @forelse($contentManagers as $key => $contentManager)
+                                        <tr>
+                                            <td>{{ $contentManager->type ?? '' }}</td>
+                                            <td>
+                                                <a href="{{ route('admin.content.edit', $contentManager->id) }}" class="btn btn-sm btn-info"><i class="fas fa-pencil-alt"></i></a>
+                                            </td>
+                                        </tr>
+                                    @empty
+                                    @endforelse
+                                    </tbody>
+                                </table>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                        <!-- /.card -->
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+        </section>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+
+@endsection
