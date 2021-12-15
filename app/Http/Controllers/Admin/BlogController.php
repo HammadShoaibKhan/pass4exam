b2515003cd4184324d9fdea992d7e56531c32dc8
HAFIZ MUHAMMAD AHTSHAM AHMAD ZAHOOR <61229568+ahtshambhatti@users.noreply.github.com>
2021-12-15 11:35:06 +0500

    Update BlogController.php

--- app/Http/Controllers/Admin/BlogController.php
+++ app/Http/Controllers/Admin/BlogController.php
@@ -4,15 +4,34 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Blog;
 
 class BlogController extends Controller
 {
     function index(){
         $title = "Blogs";
-        // $blogs = blog::orderBY('id', 'DESC')->get();
-        $blogs = array();
+        $blogs = Blog::orderBY('id', 'DESC')->get();
         return view('admin.blogs.index', compact('title','blogs'));
     }
+
+    public function create()
+    {
+        $title = "Blog";
+        $blog = Blog::orderBy('title', 'ASC')->whereStatus(1)->select('id', 'title')->get();
+        return view('admin.blogs.create', compact('title', 'blog'));
+    }
+
+    public function checkNameExists(Request $request)
+    {
+        if (!empty($request->title)) {
+            if (Blog::where('title', $request->title)->exists()) {
+                return "false";
+            }
+            return "true";
+        }
+        return "true";
+    }
+
 
     function blogView($id=null){
         // if ($id != null && blog::where('id', $id)->exists()) {
