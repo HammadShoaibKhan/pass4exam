8dbd275948bff074375023fce6af41c2696c6429
HAFIZ MUHAMMAD AHTSHAM AHMAD ZAHOOR <61229568+ahtshambhatti@users.noreply.github.com>
2021-12-19 00:13:35 +0500

    Update index.blade.php

--- resources/views/admin/blogs/index.blade.php
+++ resources/views/admin/blogs/index.blade.php
@@ -47,30 +47,26 @@
                                     <thead>
                                     <tr>
                                         <th><input type="checkbox" class="select-all-checkbox"></th>
-                                        <th>Name</th>
-                                        <th>Email</th>
-                                        <th>Subject</th>
+                                        <th>Title</th>
                                         <th>Approved</th>
                                         <th>Action</th>
                                     </tr>
                                     </thead>
                                     <tbody>
-                                    @forelse($blogs as $key => $testimonial)
+                                    @forelse($blogs as $key => $blog)
                                         <tr>
-                                            <td><input type="checkbox" class="checkboxes" data-id="{{ $testimonial->id }}"></td>
-                                            <td>{{ $testimonial->name ?? '' }}</td>
-                                            <td>{{ $testimonial->email ?? '' }}</td>
-                                            <td>{{ $testimonial->subject ?? '' }}</td>
+                                            <td><input type="checkbox" class="checkboxes" data-id="{{ $blog->id }}"></td>
+                                            <td>{{ $blog->title ?? '' }}</td>
                                             <td>
-                                                @if ($testimonial->approved == 1)
+                                                @if ($blog->approved == 1)
                                                     <span class="badge badge-success">Active</span>
                                                 @else
                                                     <span class="badge badge-danger">Disabled</span>
                                                 @endif
                                             </td>
                                             <td>
-                                                <a href="{{ route('admin.testimonial.view', $testimonial->id) }}" class="btn btn-sm btn-info"><i class="fas fa-eye"></i></a>
-                                                <a href="javascript:;" data-id="{{ $testimonial->id }}" data-route="{{ route('admin.testimonial.delete') }}" class="btn btn-sm btn-danger del-testimonial"><i class="fas fa-trash-alt"></i></a>
+                                                <a href="{{ route('admin.blog.view', $blog->id) }}" class="btn btn-sm btn-info"><i class="fas fa-eye"></i></a>
+                                                <a href="javascript:;" data-id="{{ $blog->id }}" data-route="{{ route('admin.blog.delete') }}" class="btn btn-sm btn-danger del-blog"><i class="fas fa-trash-alt"></i></a>
                                             </td>
                                         </tr>
                                     @empty
