25cab0dab7f1bc487fe99d7a921286028b2b422b
HAFIZ MUHAMMAD AHTSHAM AHMAD ZAHOOR <61229568+ahtshambhatti@users.noreply.github.com>
2021-12-25 11:46:27 +0500

    fixes

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -34,15 +34,15 @@
         <!-- Main content -->
         <section class="content">
             {{-- <!------------- Blogs List --------------------------------------------> --}}
-            <section class="exam_vendor_testimonials pt-5 pb-5" style=" ">
-                <div id="sample_page_2_new_testimonials" class="col-xl-12 col-lg-12 col-md-12 col-sm-12 mt-3 mb-3">
+            <section class="exam_vendor_testimonials row pt-5 pb-5" style=" ">
+                <div id="sample_page_2_new_testimonials" class="col-xl-7 col-lg-7 col-md-7 col-sm-7 mt-3 mb-3">
                     <div id="parant_comment" class="container mt-3">
                         @forelse ($blogs as $blog)
                             <div class="media p-3">
                                 <div class="media-body">
                                     <h3 class="p-2">{{ strtoupper($blog->title) ?? '' }}</h3>                          
                                     <p class="pl-4" style="color: #22ad95;font-size: 16px;font-weight: 400;">
-                                        <?php echo (Str::substr(strip_tags($blog->description),0,500)) ?? ''?>
+                                        <?php echo (Str::substr(strip_tags($blog->description),0,300)) ?? ''?>
                                         ...<a href="{{ route('blog_view',$blog->slug)}}" class=""> Read more</a>
                                     </p>
                                     <div class="rating">
@@ -53,6 +53,18 @@
                         @empty
                             
                         @endforelse  
+                    </div>
+                </div>
+                <div class="col-sm-1"></div>
+                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 mt-3 p-3" style="border:1px solid #22ad95;border-radius:20px;">
+                    <h3>Blogs</h3>
+                    <div class="list-group">                        
+                        @forelse ($blogs as $blog)
+                            <a href="{{ route('blog_view',$blog->slug)}}" class="list-group-item list-group-item-action p-3">
+                                {{ strtoupper($blog->title) ?? ''}}
+                            </a>                            
+                        @empty
+                        @endforelse
                     </div>
                 </div>
             </section>
