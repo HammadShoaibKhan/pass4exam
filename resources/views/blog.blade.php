4f85792e737d01a81115b93dfdfece14b5ebb2d2
HAFIZ MUHAMMAD AHTSHAM AHMAD ZAHOOR <61229568+ahtshambhatti@users.noreply.github.com>
2021-12-25 10:28:04 +0500

    Update blog.blade.php

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -40,22 +40,26 @@
         <!-- Main content -->
         <section class="content">
             {{-- <!------------- Blogs List --------------------------------------------> --}}
-            <section class="exam_vendor_testimonials pt-5 pb-5" style=" ">
-                <div id="sample_page_2_new_testimonials" class="col-xl-12 col-lg-12 col-md-12 col-sm-12 mt-3 mb-3">
-                    <div id="parant_comment" class="container mt-3">
+            <section class="exam_vendor_testimonials pb-5" style=" ">
+                <div id="sample_page_2_new_testimonials" class="col-xl-12 col-lg-12 col-md-12 col-sm-12 mb-3">
+                    <div id="parant_comment" class="container">
                         <div class="media p-3">
                             <div class="media-body">
-                            
-                                <div class="image">
-                                    @empty(!$blog->title)
-                                      {{ ucfirst( Str::substr($blog->title, 0, 1) ) ?? '' }}                          
-                                    @endempty
+                                <div class="row p-2">
+                                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12" style="">
+                                        <!-- Image Box -->
+                                            <img class="img-fluid rounded" id="blog_banner"
+                                                src="{{ asset('storage/blog_banner_files/' . getMediaFile('blogs','blog_banner_file', $blog->id)) }}"
+                                                style="width:100%; height: 350px;" 
+                                            >
+                                        <!-- END Image Box -->
+                                    </div>
                                 </div>
 
-                                <h3 class="p-2">{{ strtoupper($blog->title) ?? '' }}</h3>                          
+                                <h1 class="p-2 mt-2">{{ strtoupper($blog->title) ?? '' }}</h1>
+                                                          
                                 <p class="pl-4" style="color: #22ad95;font-size: 16px;font-weight: 400;">
-                                    <?php echo (Str::substr(strip_tags($blog->description),0,300)) ?? ''?>
-                                    ...<a href="{{ route('blog_view',$blog->slug)}}" class=""> Read more</a>
+                                    <?php echo ($blog->description) ?? ''?>
                                 </p>
                                 <div class="rating">
                                     <span style="float: right;font-size: 14px; color:#22ad95;"><i>Published:</i> <i style="color: #da0606;" >{{date('M d, Y', strtotime($blog->created_at))}}</i></span>
