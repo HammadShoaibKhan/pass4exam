8f19055eabd570a8dc858768cb5ef4ff9ed3a467
HAFIZ MUHAMMAD AHTSHAM AHMAD ZAHOOR <61229568+ahtshambhatti@users.noreply.github.com>
2021-12-25 10:29:24 +0500

    Update blog.blade.php

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -48,17 +48,17 @@
                                 <div class="row p-2">
                                     <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12" style="">
                                         <!-- Image Box -->
-                                            <img class="img-fluid rounded" id="blog_banner"
-                                                src="{{ asset('storage/blog_banner_files/' . getMediaFile('blogs','blog_banner_file', $blog->id)) }}"
-                                                style="width:100%; height: 350px;" 
-                                            >
+                                        <img class="img-fluid rounded" id="blog_banner"
+                                            src="{{ asset('storage/blog_banner_files/' . getMediaFile('blogs','blog_banner_file', $blog->id)) }}"
+                                            style="width:100%; height: 350px;" 
+                                        >
                                         <!-- END Image Box -->
                                     </div>
                                 </div>
 
                                 <h1 class="p-2 mt-2">{{ strtoupper($blog->title) ?? '' }}</h1>
-                                                          
-                                <p class="pl-4" style="color: #22ad95;font-size: 16px;font-weight: 400;">
+
+                                <p class="pl-4" style="font-size: 16px;font-weight: 400;">
                                     <?php echo ($blog->description) ?? ''?>
                                 </p>
                                 <div class="rating">
